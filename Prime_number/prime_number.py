5d67a7783b7cdf3b8bb4ba281a4c9bfbbecf815a
Edward Oganezov <eddyssintax@gmail.com>
2023-12-03 23:12:23 +0400

    Update prime_number.py

--- Prime_number/prime_number.py
+++ Prime_number/prime_number.py
@@ -1,10 +1,13 @@
-num = [10, 5, 89, 562, 22, 13, 17, 7, 155, 63, 89]
+nums = [23,4,5,7,13,12]
 
-for x in num:
-    if x > 1:
-        for i in range(2, x):
-            if x % i == 0:
-                print(f"{x} is not a prime number")
+for num in nums:
+
+    if num > 1:
+        for i in range(2, int(num/2)+1):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
                 break
         else:
-            print(f"{x} is a prime number")
+            print(num, "is a prime number")
+    else:
+        print(num, "is not a prime number")
